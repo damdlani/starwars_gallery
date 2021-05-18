3eaea4fe8086065129a9ff3591a42141242afce4
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2021-05-18 19:04:16 +0200

    feat(): page status handling

--- js/index.js
+++ js/index.js
@@ -1,38 +1,91 @@
-let people = {};
-let images = [];
-let currentPage = 1;
+{
+  const SWAPI_URL = "http://swapi.dev/api/people/";
+  const IMAGES_API_URL = "https://akabab.github.io/starwars-api/api/all.json";
 
-const fetchCharacters = async (page) => {
-  const response = await fetch(`http://swapi.dev/api/people/?page=${page}`);
-  const { count, next, previous, results } = await response.json();
-
-  people = {
-    ...people,
-    count,
-    nextPage: next,
-    previousPage: previous,
-    characters: results,
+  const status = {
+    loading: true,
+    error: false,
   };
 
-  findCharacterImage(people.characters[1]);
-};
-const fetchImages = async () => {
-  const response = await fetch(
-    `https://akabab.github.io/starwars-api/api/all.json`
-  );
-  const data = await response.json();
-  images = data.map(({ name, image }) => {
-    return { name, image };
-  });
-};
+  let characters = {};
+  let images = [];
+  let currentPage = 1;
 
-const findCharacterImage = (character) => {
-  const image = images.find(({ name }) => {
-    return name === character.name;
-  }).image;
+  const fetchData = async (directory) => {
+    try {
+      const response = await fetch(directory);
 
-  return image;
-};
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
 
-fetchCharacters(1);
-fetchImages();
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      renderStatus({
+        ...status,
+        loading: false,
+        error: true,
+      });
+    }
+  };
+
+  const populateCharacters = async (page) => {
+    const { count, next, previous, results } = await fetchData(`${SWAPI_URL}?page=${page}`);;
+
+    characters = {
+      ...characters,
+      count,
+      nextPage: next,
+      previousPage: previous,
+      characters: results,
+    };
+    
+    renderStatus({
+      ...status,
+      loading: false,
+      error: false,
+    });
+  };
+
+  const fetchImages = async () => {
+    const data = await fetchData(IMAGES_API_URL);
+    
+    images = data.map(({ name, image }) => {
+      return { name, image };
+    });
+  };
+
+  const findCharacterImage = ({ name: characterName }) => {
+    const { image } = images?.find(({ name: imageName }) => {
+      return imageName === characterName;
+    });
+    console.log(image);
+    return image;
+  };
+
+  const renderStatus = ({ loading, error }) => {
+    const statusElement = document.querySelector(".status");
+
+    if (loading) {
+      statusElement.classList.add("status--loading");
+      statusElement.innerHTML = "Loading data is...";
+    }
+
+    if (error) {
+      statusElement.classList.remove("status--loading");
+      statusElement.classList.remove("status--hidden");
+      statusElement.innerHTML = "occured error an - try please again";
+    }
+
+    if (!loading && !error) {
+      statusElement.classList.remove("status--loading");
+      statusElement.classList.add("status--hidden");
+      statusElement.innerHTML = "";
+    }
+  };
+  renderStatus(status);
+  populateCharacters(1);
+  fetchImages();
+}
